dc38cea69ec807efaf7f1e148a9e2946ebab5863
Michael <mwu250@gmail.com>
2024-10-31 23:52:39 -0700

    feat: demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import mhmeat from "./images/mhmeat.png";
 
 function App() {
+  const [monster, setMonter] = useState("");
+  const [monsterData, setMonsterData] = useState([]);
+
+  const ENDPOINT = "https://mhw-db.com/monsters";
+
+  // this is a simple axios get request to the endpoint which returns all the monsters
+  useEffect(() => {
+    axios
+      .get(ENDPOINT)
+      .then((res) => {
+        setMonsterData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  //
+
+  // defined functions to get monster by id
+  async function getMonster(id) {
+    try {
+      const res = await axios.get(`${ENDPOINT}/${id}`);
+      setMonsterData([res.data]);
+      setMonter("");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // function to get all monsters
+  async function getAllMonsters() {
+    try {
+      const res = await axios.get(ENDPOINT);
+      setMonsterData(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="header">
+        <img src={mhmeat} alt="Monster Hunter Meat" />
+        <h1>API Demo</h1>
+      </div>
+      <div>
+        <input
+          type="number"
+          value={monster}
+          onChange={(e) => setMonter(e.target.value)}
+          placeholder="Enter Monster ID"
+        />
+        <button
+          onClick={() => getMonster(monster)}
         >
-          Learn React
-        </a>
-      </header>
+          Get Monster by ID
+        </button>
+        <button onClick={getAllMonsters}>Get All Monsters</button>
+        <pre>{JSON.stringify(monsterData, null, 2)}</pre>
+      </div>
     </div>
   );
 }
